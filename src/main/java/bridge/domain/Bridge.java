8fd76dcdb8dddf88dd2871151b06417d17749498
sohyun95277 <sohyun95277@gmail.com>
2023-12-13 23:06:52 +0900

    feat(Bridge): 정답 다리를 갖고 player와 비교하는 기능

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,42 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> answer;
+
+    public Bridge(List<String> answer) {
+        this.answer = answer;
+    }
+
+    public boolean isAllClear(Player player) {
+        List<Move> playerMoves = player.getMoves();
+        if (isValidMoves(playerMoves) && isEndOfBridge(playerMoves.size())) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isValidMoves(List<Move> moves) {
+        for (Move move : moves) {
+            String command = move.getCommand();
+            if (isWrongMove(command)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isWrongMove(String command) {
+        for (String answerCommand : answer) {
+            if (!command.equals(answerCommand)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isEndOfBridge(int moveCount) {
+        return answer.size() == moveCount;
+    }
+}
