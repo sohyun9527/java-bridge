ddef03c2f375234bc2dcef7dd4b6ce57ba57fb3f
sohyun95277 <sohyun95277@gmail.com>
2023-12-13 23:07:14 +0900

    feat(BridgeGame): 게임 종료를 판별하는 기능

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,31 @@
 package bridge;
 
+import bridge.domain.Bridge;
+import bridge.domain.Condition;
+import bridge.domain.Move;
+import bridge.domain.Player;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final Bridge bridge;
+    private final Player player;
+
+    public BridgeGame(Bridge bridge, Player player) {
+        this.bridge = bridge;
+        this.player = player;
+    }
+
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(Move move) {
+        player.move(move);
+        return bridge.isAllClear(player);
     }
 
     /**
@@ -18,6 +33,11 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(Condition condition) {
+        if (condition == Condition.RETRY) {
+            player.restartGame();
+            return true;
+        }
+        return false;
     }
 }
