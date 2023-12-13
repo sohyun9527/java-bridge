b0f871d059336d136e504c272563251d8516d0ba
sohyun95277 <sohyun95277@gmail.com>
2023-12-13 22:04:27 +0900

    feat(Move): 입력값이 U, D 둘중하나인지 판별하는 기능

--- src/main/java/bridge/domain/Move.java
+++ src/main/java/bridge/domain/Move.java
@@ -2,24 +2,31 @@ package bridge.domain;
 
 import java.util.Arrays;
 
-public enum MoveCommand {
+public enum Move {
     UP("U", 1),
     DOWN("D", 0);
 
     private final String command;
     private final int number;
 
-    MoveCommand(String command, int number) {
+    Move(String command, int number) {
         this.command = command;
         this.number = number;
     }
 
     public static String findByNumber(int number) {
         return Arrays.stream(values())
-                .filter(moveCommand -> moveCommand.number == number)
+                .filter(move -> move.number == number)
                 .findFirst()
-                .map(MoveCommand::getCommand)
+                .map(Move::getCommand)
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 번호입니다."));
+    }
+
+    public static Move of(String moveCommand) {
+        return Arrays.stream(values())
+                .filter(command -> command.command.equals(moveCommand))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] U(위), D(아래)로만 움직일 수 있습니다."));
     }
 
     public String getCommand() {
---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,48 @@
+package bridge.view;
+
+import bridge.domain.Move;
+import bridge.validation.InputValidation;
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+    private static final String REQUEST_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String REQUEST_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String REQUEST_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        System.out.println(REQUEST_SIZE);
+        String input = getUserInput();
+        InputValidation.validateBridgeLength(input);
+        return Integer.parseInt(input);
+    }
+
+    public Move readMoveCommand() {
+        System.out.println(REQUEST_MOVE);
+        String input = getUserInput();
+        return Move.of(input);
+    }
+
+    private String getUserInput() {
+        return Console.readLine();
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        return null;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        return null;
+    }
+}
